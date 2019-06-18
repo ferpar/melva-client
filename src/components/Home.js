bfa5946076a0f55fa788ed73ece29f3bab55d472
fer <ferp@protonmail.ch>
2019-06-18 16:24:20 +0200

    added basic formik + yup form @ Home.js component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,63 @@
 import React, { useState } from "react";
 import { withRouter } from "react-router-dom";
 import axios from "axios";
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
-const Home = props => {
-  const [state, setState] = useState({ name: "", surname: "", phone: "" });
+const BaseHome = ({
+  values, 
+  errors,
+  touched,
+  isSubmitting,
+  handleLogin
+}) => (
+    <div className="home-container">
+      <h1 className="main title">Bienvenido!</h1>
+      <Form className="login-signup-guest" >
+        { touched.name && errors.name && <p>{ errors.name }</p> }
+        <label htmlFor="name">nombre</label>
+        <Field
+          name="name"
+          type="text"
+        />
+        <br />
 
-  const handleChange = ({ name, value }) => {
-    setState({ ...state, [name]: value });
-  };
+        <label htmlFor="surname">apellido</label>
+        <Field
+          name="surname"
+          type="text"
+        />
+        <br />
+        { touched.phone && errors.phone && <p>{ errors.phone }</p> } 
+        <label htmlFor="phone">teléfono</label>
+        <Field
+          name="phone"
+          type="text"
+        />
+        <br />
 
-  const handleSubmit = e => {
-    e.preventDefault();
+        <button disabled={isSubmitting} type="submit">Entrar</button>
+      </Form>
+    </div>
+  )
+
+const Home = withFormik({
+  mapPropsToValues(){
+    return{
+      name: '',
+      surname: 'b',
+      phone: ''
+    }
+  },
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required('must enter a name'),
+    surname: Yup.string(),
+    phone: Yup.string().min(4,'must be at least 4 digits long').required('must enter a phone')
+  }),
+  handleSubmit(values, {props, setSubmitting }) {
+    console.log(values)
     axios
-      .post("http://localhost:3010/api/auth/login-signup-guest", state, {
+      .post("http://localhost:3010/api/auth/login-signup-guest", values, {
         headers: {
           "Content-Type": "application/json; charset=utf-8",
           Accept: "application/json"
@@ -24,43 +69,8 @@ const Home = props => {
         props.handleLogin(result.data, true, "/appointments");
       })
       .catch(err => console.error("there was an error posting your info", err));
-  };
+    setSubmitting(false)
+  }
+})(BaseHome)
 
-  return (
-    <div className="home-container">
-      <h1 className="main title">Bienvenido!</h1>
-
-      <form className="login-signup-guest" onSubmit={e => handleSubmit(e)}>
-        <label htmlFor="name">nombre</label>
-        <input
-          name="name"
-          type="text"
-          onChange={e => handleChange(e.target)}
-          value={state.name}
-        />
-        <br />
-
-        <label htmlFor="surname">apellido</label>
-        <input
-          name="surname"
-          type="text"
-          onChange={e => handleChange(e.target)}
-          value={state.surname}
-        />
-        <br />
-
-        <label htmlFor="phone">teléfono</label>
-        <input
-          name="phone"
-          type="text"
-          onChange={e => handleChange(e.target)}
-          value={state.phone}
-        />
-        <br />
-
-        <button type="submit">Entrar</button>
-      </form>
-    </div>
-  );
-};
 export default withRouter(Home);
