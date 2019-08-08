399ad7ee975f052843140d642ced165eb5debb2d
fer <ferp@protonmail.ch>
2019-08-08 18:24:24 +0200

    using env variable on the dev-server as well

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,12 +1,26 @@
+const webpack = require('webpack')
 const merge = require("webpack-merge");
 const baseConfig = require("./webpack.config.base");
 
-module.exports = merge(baseConfig, {
+module.exports = (env) => {
+
+  const envKeys = Object.keys(env).reduce((accu, variable) => {
+    accu[`process.env.${variable}`] = JSON.stringify(env[variable]);
+    return accu;
+  } ,{});
+  
+  return merge(baseConfig, {
   mode: "development",
   devServer: {
     host: '192.168.1.51',
     port: 9000,
     historyApiFallback: true
   },
-  devtool: "source-map"
+  devtool: "source-map",
+  plugins: [
+    new webpack.DefinePlugin(envKeys)
+  ]
+
 });
+
+}
