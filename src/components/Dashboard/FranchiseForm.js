bfdd5e21e28a7669fabbcdc8ec8d924d6f163251
fer <ferp@protonmail.ch>
2019-10-18 16:08:09 +0200

    got the Formik integration working on the FranchiseForm component, also separated its css classes

--- src/components/Dashboard/FranchiseForm.js
+++ src/components/Dashboard/FranchiseForm.js
@@ -4,7 +4,12 @@ import "./FranchiseForm.css";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 
-const BaseForm = () => (
+const BaseForm = ({
+  values,
+  errors,
+  touched,
+  isSubmitting
+}) => (
   <div className="franchise-form-container">
     <div className="franchise-form-wrapper">
       <h1 className="franchise-title">Datos de la franquicia</h1>
@@ -13,14 +18,22 @@ const BaseForm = () => (
           <div className="franchise-field-wrapper fra-name">
             <label htmlFor="name">nombre</label>
             <Field name="name" type="text" />
+            {touched.name && errors.name && (
+              <p className="fra-error-msg">{errors.name}</p>  
+            )}
           </div>
 
           <div className="franchise-field-wrapper fra-sms-name">
-            <label htmlFor="sms-name">nombre abreviado</label>
-            <Field name="sms-name" type="text" />
+            <label htmlFor="sms">nombre abreviado</label>
+            <Field name="sms" type="text" />
+            {touched.sms && errors.sms && (
+              <p className="fra-error-msg">{errors.sms}</p>
+            )}
           </div>
 
           <div className="fra-buttons-wrapper">
+            {console.log(errors)}
+            {console.log(touched)}
             <button
               className="cancel-button"
  //             onClick={() => setEditMode(false)}
@@ -28,7 +41,7 @@ const BaseForm = () => (
             >
               Cancelar
             </button>
-            <button className="fra-edit-button" /*disabled={isSubmitting}*/ type="submit">
+            <button className="fra-edit-button" disabled={isSubmitting} type="submit">
               Guardar
             </button>
           </div>
@@ -39,6 +52,23 @@ const BaseForm = () => (
 )
 
 const FranchiseForm = withFormik({
+  mapPropsToValues() {
+    return {
+      name: "",
+      sms: ""
+    };
+  },
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required("por favor, introduzca nombre de la franquicia"),
+    "sms": Yup.string()
+      .min(3, "mínimo 3 caracteres")
+      .max(11, "máximo 11 caracteres")
+      .required("Este nombre encabeza los SMS/mensajes a pacientes")
+  }),
+  handleSubmit(values, {props, setSubmitting}) {
+    console.log(values);
+    setSubmitting(false);
+  }
 })(BaseForm);
 
 export default FranchiseForm;
