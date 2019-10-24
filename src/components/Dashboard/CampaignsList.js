490b42897a9ecb74777f8dae9b0f82c8b872dfd6
fer <ferp@protonmail.ch>
2019-10-24 18:06:15 +0200

    corrected typo naming a css file and its corresponding reference in a .js file

--- src/components/Dashboard/CampaignsList.js
+++ src/components/Dashboard/CampaignsList.js
@@ -1,25 +1,23 @@
 import React from "react";
-import "./CmapaignsList.css";
+import "./CampaignsList.css";
 
 const CampaignsList = props => {
-
   return (
     <div className="camplist-container">
       <div className="camplist-wrapper">
         <h1 className="camplist-title">Campaigns</h1>
         {props.franchise.campaigns ? (
-        props.franchise.campaigns.map( (campaign, ind) => (
-          <p key={ind}>{campaign.title}</p>
-        ))
+          props.franchise.campaigns.map((campaign, ind) => (
+            <p key={ind}>{campaign.title}</p>
+          ))
         ) : (
           <p> No campaigns created yet </p>
         )}
-        <hr className="camplist-separator"/>
+        <hr className="camplist-separator" />
         <button className="camplist-button">Campaign Manager</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CampaignsList
-
+export default CampaignsList;
