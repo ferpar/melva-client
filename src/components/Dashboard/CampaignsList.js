b193adcaaf80899d6bda9b94684abc024ee51abc
fer <ferp@protonmail.ch>
2019-10-24 18:36:37 +0200

    Created Link to Campaign Manager at the CampaignsList component

--- src/components/Dashboard/CampaignsList.js
+++ src/components/Dashboard/CampaignsList.js
@@ -1,20 +1,27 @@
 import React from "react";
 import "./CampaignsList.css";
 
+import { Link } from "react-router-dom";
+
 const CampaignsList = props => {
   return (
     <div className="camplist-container">
       <div className="camplist-wrapper">
-        <h1 className="camplist-title">Campaigns</h1>
+        <h1 className="camplist-title">Campañas</h1>
         {props.franchise.campaigns ? (
           props.franchise.campaigns.map((campaign, ind) => (
             <p key={ind}>{campaign.title}</p>
           ))
         ) : (
-          <p> No campaigns created yet </p>
+          <p> Ninguna campaña creada </p>
         )}
         <hr className="camplist-separator" />
-        <button className="camplist-button">Campaign Manager</button>
+        <Link 
+          to="/campaigns"
+          className="camplist-button"
+        >
+        Ir a Gestor de Campañas
+        </Link>
       </div>
     </div>
   );
