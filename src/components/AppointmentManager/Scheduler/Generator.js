bffa4677d86288420b8dad1ba21c5f4cb22075b7
fer <ferp@protonmail.ch>
2019-11-08 21:25:45 +0100

    second iteration of the schedule generator component

--- src/components/AppointmentManager/Scheduler/Generator.js
+++ src/components/AppointmentManager/Scheduler/Generator.js
@@ -1,48 +1,169 @@
-import React, { useReducer } from "react";
+import React, { useState, useReducer, useEffect } from "react";
 import "./Generator/Generator.css";
 
 import DayHeader from "./Generator/DayHeader/DayHeader.js"
 import DayList from "./Generator/DayList/DayList.js"
 
+import Flatpickr from "react-flatpickr";
+import { Spanish } from "flatpickr/dist/l10n/es.js";
+import "flatpickr/dist/themes/material_green.css";
+
 const Generator = props => { 
 
+  const [isEditingRanges, setEditingRanges] = useState(false)
+
+  const handleEditingRanges = () => {
+    setEditingRanges( !isEditingRanges )
+  }
+
+  const [rangeMin, setMinRange] = useState("08:00")
+  const [rangeMax, setMaxRange] = useState("12:00")
+
   const initialState = [
-    { day: 1, initial: "L", ranges: [{start:"8", end:"10"}] },
-    { day: 2, initial: "M", ranges: [] },
-    { day: 3, initial: "X", ranges: [] },
-    { day: 4, initial: "J", ranges: [] },
-    { day: 5, initial: "V", ranges: [] },
-    { day: 6, initial: "S", ranges: [] },
-    { day: 0, initial: "D", ranges: [] }
+    { day: 1, checked: false, initial: "L", ranges: [] },
+    { day: 2, checked: false, initial: "M", ranges: [] },
+    { day: 3, checked: false, initial: "X", ranges: [] },
+    { day: 4, checked: false, initial: "J", ranges: [] },
+    { day: 5, checked: false, initial: "V", ranges: [] },
+    { day: 6, checked: false, initial: "S", ranges: [] },
+    { day: 0, checked: false, initial: "D", ranges: [] }
   ]
   const [schedule, dispatch] = useReducer((state, action) => {
+    const index = state.findIndex( row => row.day === action.day)
+    console.log(index)
     switch (action.type) {
       case 'add':
-        state[action.day].ranges.push(action.range)
+      //  state[index].ranges.push(action.range)
+      //  state[index].ranges.sort((a,b) => a.start - b.start)
+      return [
+        ...state.slice(0, index),
+        {
+          ...state[index], 
+          ranges: 
+            [...state[index].ranges, action.range]
+            .sort((a,b) => parseInt(
+                a.start.slice(0,2) + a.start.slice(3)
+              ) - parseInt(
+                b.start.slice(0,2) + b.start.slice(3)
+              )
+            )
+        },
+        ...state.slice(index + 1)
+      ]
         return state
+      case 'checkbox':
+        return [
+          ...state.slice(0,index),
+          {...state[index], checked: !state[index].checked},
+          ...state.slice(index+1)]
       default:
         return state
     }
   }, initialState)
 
+  const handleChecked = (e, day) => {
+    dispatch({
+      type: "checkbox", day: day
+    })
+  }
+
+  const handleAddRange = (start, end) => {
+    schedule.forEach ( day => {
+      if (day.checked) {
+        dispatch({
+          type: "add",
+          day: day.day,
+          range: {
+            start,
+            end
+          }
+        })
+      }
+    })
+  }
+
+  const removeRange = (day, ind) => {
+
+  }
+
+  useEffect( () => {
+    console.log(schedule)
+  } , [schedule])
+
   return (
     <div className="generator-container">
-    { schedule.map( (day, ind) => (
-        <div 
-          className="generator-slug"
-          key={ind}
-        >
-          <DayHeader 
-            className = "dayhead"
-            initial = { day.initial }
+      <button onClick={ handleEditingRanges }
+      > 
+      { isEditingRanges ?
+        ("Cancelar") :
+        ("Editar Horario") 
+      }
+      </button>
+      { isEditingRanges && 
+        <>
+          <Flatpickr
+            options={{ 
+              locale: Spanish,
+              noCalendar:true,
+              enableTime:true,
+              dateFormat:"H:i",
+              defaultDate:"08:00"
+            }}
+            className="appointments-flatpickr"
+            placeholder="pulse aquí..."
+            onChange={e => setMinRange(e[0].toLocaleTimeString("es-ES", {hour: "2-digit", minute: "2-digit"}))}
           />
-          <DayList
-            className = "daylist"
-            ranges = {day.ranges}
+          <Flatpickr
+            options={{ 
+              locale: Spanish,
+              noCalendar:true,
+              enableTime:true,
+              dateFormat:"H:i",
+              defaultDate:"12:00"
+            }}
+            className="appointments-flatpickr"
+            placeholder="y aquí..."
+            onChange={e => setMaxRange(e[0].toLocaleTimeString("es-ES", {hour: "2-digit", minute: "2-digit"}))}
           />
-        </div>
-      )) 
-    }
+          <button
+            onClick={() => handleAddRange(rangeMin, rangeMax)}
+          >
+            Añadir Horas
+          </button>
+        </>
+      }
+      <div className="generator-wrapper">
+      { schedule.map( (day, ind) => (
+          <div 
+            className="generator-slug"
+            key={ind}
+          >
+            <DayHeader 
+              className = "dayhead"
+              initial = {day.initial}
+              day = {day.day}
+              isEditingRanges = {isEditingRanges}
+              handleChecked = {handleChecked}
+              checked = {day.checked}
+              schedule = {schedule}
+            />
+            <DayList
+              className = "daylist"
+              ranges = {day.ranges}
+            />
+          </div>
+        )) 
+      }
+      </div>
+      {
+        isEditingRanges && (
+         <button
+          onClick={() => console.log(schedule)}
+          >
+            Generar Citas
+         </button>
+        )
+      }
     </div>
   )
 }
--- src/components/AppointmentManager/Scheduler/Generator/DayHeader/DayHeader.js
+++ src/components/AppointmentManager/Scheduler/Generator/DayHeader/DayHeader.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import "./DayHeader.css";
 
-const DayHeader = props => (
-  <div className="day-header">
-    <p>{props.initial}</p>
-  </div>
-)
+const DayHeader = props => {
+
+  return (
+    <div className="day-header">
+      { props.isEditingRanges &&
+        <input 
+          onChange={(e) => {props.handleChecked(e, props.day)
+          }}
+          type="checkbox"
+          checked = {props.checked}
+        />
+      }
+      
+      <p>{props.initial}</p>
+    </div>
+  )
+}
 
 export default DayHeader
