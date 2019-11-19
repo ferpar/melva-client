31aab0eae31b68c7eaaaf05091c14007f4103c0d
fer <ferp@protonmail.ch>
2019-11-19 10:12:00 +0100

    improvements to generator and appointmentManager styling

--- src/components/AppointmentManager/Scheduler/Generator/DayList/DayList.js
+++ src/components/AppointmentManager/Scheduler/Generator/DayList/DayList.js
@@ -5,7 +5,7 @@ const DayList = props => (
   <div className="ranges-container">
   <hr className="daylist-separator" />
   { props.ranges.map( (range, ind) => (
-    <div key={ind}>
+    <div className="daylist-range" key={ind}>
       <p>{range.start}{" - "}{range.end}</p>
       <button
         onClick={() => props.handleRemoveRange(props.day, ind)}
