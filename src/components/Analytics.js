af87ca9bbee1d1aa0ff830d0e2cd977def840474
fer <ferp@protonmail.ch>
2019-12-19 19:21:38 +0100

    first iteratin of the component, imports and processes the given info

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -11,6 +11,7 @@ const Analytics = props => {
   const { franchise } = props.user
   const [isLoading, setIsLoading] = useState(true);
   const [jsonSource, setJsonSource] = useState(null);
+  const [formattedSourceData, setFormattedSourceData] = useState(null);
 
   const [menuOpen, setMenuOpen] = useState(false);
   const handleStateChange = state => {
@@ -27,8 +28,157 @@ const Analytics = props => {
       //headers:["surname", "name", "phone"]
     })
       .fromString(csvContent)
+
     console.log(jsonObj)
     setJsonSource(jsonObj)
+
+    const afterFormatting = await formatImport(jsonObj)
+    console.log(afterFormatting)
+    setFormattedSourceData(afterFormatting)
+    
+  }
+
+  //preprocess the imported data into a usable object
+  //!!! for now this function is being manually adapted to the given data format
+  
+  const formatImport = (jsonObj) => (
+    jsonObj.map( billable => ({
+      treatment: billable.Tratamiento,
+      patientId: billable.NHC,
+      name: billable.Nombre ,
+      surname: billable.Apellidos,
+      fullname: billable.ApellidosNombre,
+      phone: billable.Telf,
+      address: billable.Direccion,
+      day: billable.Fecha.split("/")[1],
+      month: billable.Fecha.split("/")[0],
+      year: billable.Fecha.split("/")[2],
+      date: new Date(
+        billable.Fecha.split("/")[2], 
+        billable.Fecha.split("/")[0] - 1,
+        billable.Fecha.split("/")[1] )
+    }))  
+  )
+
+  const groupBy = (formattedData, criterion = "year") => {
+
+    const groupedBills = {}
+
+    formattedData.forEach( bill => {
+      if (groupedBills.hasOwnProperty(bill[criterion])) {
+        groupedBills[bill[criterion]].push(bill)
+      } else {
+        groupedBills[bill[criterion]] = [bill]
+      }
+    })
+
+    return groupedBills
+  }
+
+  const groupByMonth = (formattedData, criterion = "year") => {
+    
+    const groupedBills = groupBy(formattedData, criterion)
+    const nestedGroupBills = {}
+
+    for (year in groupedBills) {
+      nestedGroupBills[year] = groupBy(groupedBills[year], "month")
+    }
+
+    return nestedGroupBills
+
+  }
+
+
+  const groupByDay = (formattedData, criterion = "year") => {
+    
+    const groupedBills = groupBy(formattedData, criterion)
+    const nestedGroupBills = {}
+
+    for (year in groupedBills) {
+      nestedGroupBills[year] = groupBy(groupedBills[year], "month")
+      for (month in nestedGroupBills[year]) {
+        nestedGroupBills[year][month] = groupBy(nestedGroupBills[year][month], "day")
+      }
+    }
+
+    return nestedGroupBills
+
+  }
+
+  const groupByYearPatient = (formattedData, criterion = "year") => {
+    
+    const groupedBills = groupBy(formattedData, criterion)
+    const nestedGroupBills = {}
+
+    for (year in groupedBills) {
+      nestedGroupBills[year] = groupBy(groupedBills[year], "fullname")
+    }
+
+    return nestedGroupBills
+  }
+
+  const generatePatientBase = (formattedData) => {
+    const patientList = {}
+    formattedData.forEach ( bill => {
+      if (patientList.hasOwnProperty(bill.patientId)) {
+        patientList[bill.patientId].bills.push(bill)
+        patientList[bill.patientId].years.add(bill.year)
+        patientList[bill.patientId].yearSum = patientList[bill.patientId].years.size
+        const years = [] 
+        patientList[bill.patientId].years.forEach(year => years.push(year))
+        patientList[bill.patientId].yearSpan = (Math.max(...years) - Math.min(...years))
+        patientList[bill.patientId].billSum = patientList[bill.patientId].bills.length
+      } else {
+        patientList[bill.patientId]= {
+          bills: [bill], 
+          fullname: bill.fullname,
+          years: new Set([bill.year]),
+          yearSum: 1,
+          yearSpan: 1,
+          billSum: 1
+        }
+      }
+    })
+    return patientList
+  }
+
+  // example of rankParameters: { "Ayears": 8, "Byears": 5, "Abills": 20, "Bbills": 10 }}
+
+  const rankPatientBase = (patientBase, rankParameters, yearSumToggle = false) => {
+    const rankedPatientBase = {...patientBase}
+      
+      for (patient in patientBase) {
+
+        //longevity ranking
+        if (yearSumToggle) {
+          if (patientBase[patient].yearSum > rankParameters.Ayears) {
+            rankedPatientBase[patient].longevityRank = "A"
+          } else if (patientBase[patient].yearSum > rankParameters.Byears) {
+            rankedPatientBase[patient].longevityRank = "B"
+          } else {
+            rankedPatientBase[patient].longevityRank = "C";
+          }
+        } else {
+          if (patientBase[patient].yearSpan > rankParameters.Ayears) {
+            rankedPatientBase[patient].longevityRank = "A"
+          } else if (patientBase[patient].yearSpan > rankParameters.Byears) {
+            rankedPatientBase[patient].longevityRank = "B"
+          } else {
+            rankedPatientBase[patient].longevityRank = "C";
+          }
+        }
+
+        //asiduity ranking
+        if (patientBase[patient].billSum > rankParameters.Abills) {
+          rankedPatientBase[patient].asiduityRank = "A"
+        } else if (patientBase[patient].billSum > rankParameters.Bbills) {
+          rankedPatientBase[patient].asiduityRank = "B"
+        } else {
+          rankedPatientBase[patient].asiduityRank = "C";
+        }
+      }
+
+    return rankedPatientBase
   }
 
   useEffect( () => {
@@ -69,7 +219,7 @@ const Analytics = props => {
                       (
                         jsonSource.slice(0, 10).map( (unit, idx) => 
                           <div key={idx} className="analytics-list-slug">
-                            <p>{unit.NºHC}</p>
+                            <p>{unit.NHC}</p>
                             <p>{unit.Nombre}</p>
                             <p>{unit.Apellidos}</p>
                           </div>)
